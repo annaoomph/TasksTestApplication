d93bcf919ad3b599dc1cc9cfcd88811f8a2f17f2
annaoomph <anchouskochesh@mail.ru>
2017-04-10 17:07:42 +0300

    created export files

---
+++ app/src/main/java/com/example/annakocheshkova/testapplication/UI/Activity/ExportActivity.java
@@ -0,0 +1,15 @@
+package com.example.annakocheshkova.testapplication.UI.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.annakocheshkova.testapplication.R;
+
+public class ExportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_export);
+    }
+}
